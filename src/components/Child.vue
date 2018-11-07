300eea3ab5688899f78cb92ea897817cfbaf2dd7
zzsuyifeng <zzsuyifeng@163.com>
2018-11-07 10:35:35 +0800

    first commit

---
+++ src/components/Child.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="child">
+
+    <!-- 父组件向子组件传值-子组件接收值 -->
+    <div><span>子组件：{{fromFather}}</span></div>
+    
+    <!-- 子组件向父组件传值 -->
+    <!-- <div><span>子组件：</span><button v-on:click="childValueAction">提交</button></div> -->
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  props:{
+    fromFather: String,//在父组件中接收值
+    required: true
+  },
+  data () {
+    return {
+      childValue:'我是子组件中的值'
+    }
+  },
+  methods:{
+    childValueAction(){
+      // 第一个参数fromChildValue是父组件v-on的监听方法
+      this.$emit('fromChildValue',this.childValue)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
---
+++ src/components/Father.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="father">
+    <h1>父组件子组件相互传值</h1>
+    <h5>简单 / 易用 / 便捷</h5>
+    <h5>1:父组件向子组件传值</h5>
+    <div><span>父组件：</span><input type="text" v-model="inputValue"></div>
+    <child :fromFather="inputValue"></child>
+
+     <!-- <h5>2:子组件向父组件传值</h5>
+    <div><span>父组件：{{fromChild}}</span></div>
+    <child v-on:fromChildValue="fromChildValueAction"></child> -->
+
+  </div>
+</template>
+
+<script>
+//引入子组件
+import Child from '../components/Child.vue'
+export default {
+  name: 'Father',
+  components:{
+    'child':Child
+  },
+  data () {
+    return {
+      inputValue:'',
+      fromChild:''
+    }
+  },
+  methods:{
+    // 子组件传过来的值
+    fromChildValueAction(text){
+      this.fromChild = text
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
